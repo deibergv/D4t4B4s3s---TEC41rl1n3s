41d1b1effcbf48a07c8313894f98b8922ea94997
Deiber <deigranados@ic-itcr.ac.cr>
2019-04-15 23:36:00 -0600

    Era este el arreglo

--- TECAirlinesDatabase.sql
+++ TECAirlinesDatabase.sql
@@ -1,5 +1,5 @@
 -- CREATE DATABASE TECAirlines
-USE TECAirlines
+-- USE TECAirlines
 
 CREATE TABLE Tiquete (
 	ID INTEGER NOT NULL,
@@ -15,8 +15,8 @@ CREATE TABLE Escala (
 
 CREATE TABLE Pasajero (
   Pasaporte INTEGER NOT NULL,
-  Nombre VARCHAR NOT NULL,
-  Estado VARCHAR NOT NULL,
+  Nombre VARCHAR(50) NOT NULL,
+  Estado VARCHAR(50) NOT NULL,
   Reservacion INTEGER NOT NULL,
   Tiquete BIT NOT NULL,
   CantidadMaletas INTEGER NOT NULL,
@@ -25,24 +25,24 @@ CREATE TABLE Pasajero (
 );
 
 CREATE TABLE Aeropuerto (
-  Nombre VARCHAR(15) NOT NULL
+  Nombre VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Vuelo (
-  Pasajeros VARCHAR NOT NULL,
+  Pasajeros VARCHAR(50) NOT NULL,
   CantMaletas INTEGER NOT NULL,
   Ruta INTEGER NOT NULL,
   Fecha INTEGER NOT NULL,
-  Destino VARCHAR NOT NULL,
+  Destino VARCHAR(50) NOT NULL,
   PesoTotal INTEGER NOT NULL
 );
 
 CREATE TABLE Cliente (
-  NombreComp VARCHAR NOT NULL,
+  NombreComp VARCHAR(50) NOT NULL,
   Teléfono INTEGER NOT NULL,
-  Correo VARCHAR NOT NULL,
+  Correo VARCHAR(50) NOT NULL,
   Estudiante BIT NOT NULL,
-  InfoU VARCHAR NOT NULL,
+  InfoU VARCHAR(50) NOT NULL,
   Carné INTEGER NOT NULL,
   millasEstudiante INTEGER NOT NULL,
   PRIMARY KEY(Correo)
@@ -50,39 +50,39 @@ CREATE TABLE Cliente (
 
 CREATE TABLE Asiento (
   Numero INTEGER NOT NULL,
-  Categoría VARCHAR NOT NULL
+  Categoría VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Promoción (
   Costo INTEGER NOT NULL,
-  Ruta VARCHAR NOT NULL,
-  Fecha VARCHAR NOT NULL
+  Ruta VARCHAR(50) NOT NULL,
+  Fecha VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Universidad (
-  NombreComp VARCHAR NOT NULL,
+  NombreComp VARCHAR(50) NOT NULL,
   MIllaEstudiante INTEGER NOT NULL,
   Carné INTEGER NOT NULL
 );
 
 CREATE TABLE AsistenteVuelo (
-  ID VARCHAR NOT NULL,
-  Nombre VARCHAR NOT NULL,
-  Correo VARCHAR NOT NULL,
+  ID VARCHAR(50) NOT NULL,
+  Nombre VARCHAR(50) NOT NULL,
+  Correo VARCHAR(50) NOT NULL,
   PRIMARY KEY(ID)
 );
 
 CREATE TABLE Maleta (
-  codigoM VARCHAR NOT NULL,
+  codigoM VARCHAR(50) NOT NULL,
   peso INTEGER NOT NULL,
   IDPropetario INTEGER NOT NULL,
-  InfoU VARCHAR NOT NULL,
+  InfoU VARCHAR(50) NOT NULL,
   PRIMARY KEY(codigoM)
 );
 
 CREATE TABLE Avión (
-  Tipo VARCHAR NOT NULL,
-  ID VARCHAR NOT NULL,
+  Tipo VARCHAR(50) NOT NULL,
+  ID VARCHAR(50) NOT NULL,
   AsientosDisponibles BIT NOT NULL,
   PrimClase INTEGER NOT NULL,
   EconClase INTEGER NOT NULL,
@@ -92,7 +92,10 @@ CREATE TABLE Avión (
 CREATE TABLE Reservación (
   ID INTEGER NOT NULL,
   CantidadTiquetes INTEGER NOT NULL,
-  IDPropietario VARCHAR NOT NULL,
+  IDPropietario VARCHAR(50) NOT NULL,
   PRIMARY KEY(ID)
 );
+
+
+
 
