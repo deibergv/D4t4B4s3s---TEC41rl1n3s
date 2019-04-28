7cc7413e2626f3c43f9727161b7d561b8e304613
Unknown <kisungqueso@hotmail.com>
2019-04-28 13:54:46 -0600

    Main page and additional templates for the admin page.

--- TecAirWebApp/app2/app.module.js
+++ TecAirWebApp/app2/app.module.js
@@ -4,7 +4,11 @@ angular.module('root',[
     'ngRoute',
     'ngAnimate',
     'core',
-    'home'
+    'home',
+    'core.version'
     // 'airport',
     // 'client'
-]);
+])
+.controller('MainController', ['$scope', function ($scope) {
+    $scope.loggedIn = false;
+}]);
---
+++ TecAirWebApp/app2/login/login.component.js
@@ -0,0 +1,13 @@
+'use strict'
+
+angular.
+    module('login').
+    component('login', {
+        templateUrl: 'login/login.template.html',
+        controller: [
+          function LoginController() {
+            // this.clients = Client.query();
+            // this.orderProp = 'age';
+          }
+        ]
+      });
